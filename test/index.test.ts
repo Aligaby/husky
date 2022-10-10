2f19a8aa5a04d70d8edbfc34a7c8cff64fab29d4
ionutgabrielalexa <aligaby2000@yahoo.com>
2022-10-10 14:25:39 +0300

    test 12 husky

--- test/index.test.ts
+++ test/index.test.ts
@@ -8,6 +8,6 @@ describe("Add 2 numbers", () => {
 
 describe("Substract 2 numbers", () => {
   test("check that 5 - 3 is equal to 2", () => {
-    expect(subtr(5, 3)).toBe(1);
+    expect(subtr(5, 3)).toBe(0);
   });
 });
